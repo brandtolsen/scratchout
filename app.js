20b2448e513473a32a2167929f14f5f1a85b2602
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-12-17 16:06:39 +0100

    adding mousemove event

--- app.js
+++ app.js
@@ -10,10 +10,21 @@ const shrinkCursor = function() {
     cursor.classList.remove("is-down")
 }
 
+// move the cursor based on coordinates
+const moveCursor = function (x, y) {
+    cursor.style.left = x + "px"
+    cursor.style.top = y + "px"
+}
+
 document.addEventListener("mousedown", function() {
     growCursor()
 })
 
 document.addEventListener("mouseup", function() {
     shrinkCursor()
+})
+
+document.addEventListener("mousemove", function(event) {
+    console.log(event)
+    moveCursor(event.pageX, event.pageY)
 })
